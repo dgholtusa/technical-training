80f1f5470eb1cb40d33bf09dda9c1d40e1c72592
D H <dgholtusa@gmail.com>
2022-10-10 14:11:23 +0200

    fix

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": ["security/ir.model.access",
+    "data": ["security/ir.model.access.csv",
         "views/estate_property_views.xml"
 
     ],
