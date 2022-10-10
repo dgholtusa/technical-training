1b6985ca25fe95ce22c778d13b512ed0063f9722
D H <dgholtusa@gmail.com>
2022-10-10 10:44:45 +0200

    DB

--- estate/models.py
+++ estate/models.py
@@ -3,3 +3,23 @@ from odoo import models
 class EstateProperty(models.Model):
         _name = "estate.property"
         _description = "Our Estate Module"
+
+        id = fields.Integer()
+        create_uid = fields.Integer()
+        create_date = fields.Date()
+        write_uid = fields.Integer()
+        write_date = fields.Date()
+        name = fields.Char()
+        description = fields.Char()
+        postcode = fields.Char()
+        date_availability = fields.Date()
+        expected_price = fields.Float()
+        selling_price = fields.Float()
+        bedrooms = fields.Integer()
+        living_area = fields.Integer()
+        facades = fields.Integer()
+        garage = fields.Boolean()
+        garden = fields.Boolean()
+        garden_area = fields.Integer()
+        garden_orientation = fields.Selection()
+
